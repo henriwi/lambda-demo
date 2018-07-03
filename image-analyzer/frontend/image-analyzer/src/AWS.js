2f1172d77c68ebabfb50db8415b7cb09a80a455b
Henrik Wingerei <henrik.wingerei@bekk.no>
2018-07-03 20:52:20 +0200

    Reads identity pool id from env

--- image-analyzer/frontend/image-analyzer/src/AWS.js
+++ image-analyzer/frontend/image-analyzer/src/AWS.js
@@ -2,7 +2,7 @@ import AWS from 'aws-sdk'
 
 AWS.config.region = 'eu-west-1';
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: '',
+    IdentityPoolId: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID
 });
 
 const dynamodb = new AWS.DynamoDB();
